e9031392fbbd8cf9aa34064db8425ab7b2917f71
Petros Kavvathas <fpsusername@outlook.com>
2019-06-17 13:39:27 +0200

    Updated GUI, Plot

--- stereo_calculator.py
+++ stereo_calculator.py
@@ -44,51 +44,55 @@ class StereoVisionCalculator(object):
         self.root.title("StereoVision Calculator")
         self.root.resizable(0, 0)  # Don't allow resize
 
-        self.var = [tk.IntVar(self.root) for _ in range(2)]  # Checkboxes
+        self.check = [tk.IntVar(self.root) for _ in range(2)]  # Checkboxes
         self.pmenu = [tk.StringVar(self.root) for _ in range(2)]  # Popup menus
-        self.l = [tk.DoubleVar(self.root) for _ in range(3)]  # Results
-        self.entries = [ttk.Entry(self.root) for _ in range(9)]  # Entries
-        self.entries[8]["state"] = "disabled"
-        self.entries[8]["width"] = 14
+        self.results = [tk.DoubleVar(self.root) for _ in range(5)]  # Results
+        self.entries = [ttk.Entry(self.root) for _ in range(12)]  # Entries
+        self.entries[9]["state"] = "disabled"
+        self.entries[9]["width"] = 14
+
+        self.results[3].set('0 - 0')
+        self.results[4].set('0')
 
         # Entries
         for row, entry in enumerate(self.entries, start=1):
             entry.grid(row=row, column=1, sticky="W")
 
-        # Text
-        textlist = [
-            'Sensor size', 'Resolution width', 'Resolution height',
-            'Focal FoV', 'Max depth', 'Max depth error', 'Calibration disparity error',
-            'Disparity range', 'Min disparity', 'Focal length', 'Baseline',
-            'Minimum measurable depth'
-        ]
-        symbollist = [
-            'pixels', 'pixels', '', 'm', 'm', 'pixel', 'pixels', 'pixels', 'mm', 'mm',
-            'cm'
-        ]
+        textlist = {
+            'Sensor size': '',
+            'Resolution width': 'pixels',
+            'Resolution height': 'pixels',
+            'Focal FoV': '',
+            'Min baseline': 'mm',
+            'Max baseline': 'mm',
+            'Min depth': 'mm',
+            'Performance depth': 'm',
+            'Performance depth error': 'cm',
+            'Performance disparity': 'pixels',
+            'Max disparity': 'pixels',
+            'Calibration disparity error': 'pixel',
+            'Focal length': 'mm',
+            'Baseline': 'mm',
+            'Max depth': 'm',
+            'Depth resolution': 'pixels',
+            'Depth FoV': 'deg'
+        }
 
-        i = 0
-        while i < len(textlist):
-            ttk.Label(self.root, text=textlist[i]).grid(
-                row=i + 1, sticky="W", pady=5)
-            i += 1
-
-        j = 0
-        while j < len(symbollist):
-            ttk.Label(self.root, text=symbollist[j]).grid(row=j + 2,
-                                                          column=2,
-                                                          sticky="W")
-            j += 1
+        for x, (val, key) in enumerate(textlist.items()):
+            ttk.Label(self.root, text=val).grid(
+                row=x + 1, sticky="W", pady=5)
+            ttk.Label(self.root, text=key).grid(
+                row=x + 1, column=2, sticky="W")
 
         # Buttons
         ttk.Checkbutton(self.root,
                         text="Auto calculate",
-                        variable=self.var[0],
+                        variable=self.check[0],
                         command=self._callback).grid(row=0, sticky="W", pady=5)
         ttk.Checkbutton(self.root,
                         text="",
-                        variable=self.var[1],
-                        command=self._disp).grid(row=9, column=1, sticky="E")
+                        variable=self.check[1],
+                        command=self._disp).grid(row=10, column=1, sticky="E")
         ttk.Button(self.root,
                    text="Capture",
                    width=12,
@@ -96,11 +100,11 @@ class StereoVisionCalculator(object):
         ttk.Button(self.root,
                    text="Calculate",
                    width=12,
-                   command=self._callback).grid(row=13, sticky="W")
+                   command=self._callback).grid(row=18, sticky="W")
         ttk.Button(self.root,
                    text="Plot",
                    width=12,
-                   command=self._plot).grid(row=13, column=2, sticky="W")
+                   command=self._plot).grid(row=18, column=2, sticky="W")
 
         # Dropdown menus
         choices1 = {'', 'mm', 'inch'}
@@ -114,15 +118,9 @@ class StereoVisionCalculator(object):
         self.popupMenu1.grid(row=4, column=2, sticky="W")
 
         # Results
-        ttk.Label(self.root, textvariable=self.l[0]).grid(row=10,
-                                                          column=1,
-                                                          sticky="W")
-        ttk.Label(self.root, textvariable=self.l[1]).grid(row=11,
-                                                          column=1,
-                                                          sticky="W")
-        ttk.Label(self.root, textvariable=self.l[2]).grid(row=12,
-                                                          column=1,
-                                                          sticky="W")
+        for x in range(0, len(self.results)):
+            ttk.Label(self.root, textvariable=self.results[x]).grid(
+                row=x + 13, column=1, sticky="W")
 
         col_count, row_count = self.root.grid_size()
 
@@ -136,10 +134,12 @@ class StereoVisionCalculator(object):
                                focal_fov, fov_type):
         """
         Function to calculate the focal length of the imaging sensor given:
-        :param: sensor_size: The diagonal sensor size in inch or mm, eg: 2/3in
+        :param: sensor_size: The diagonal sensor size
+        :param: size: The measurement system of the sensor (metric/imperial)
         :param: img_width: The amount of pixels in width
         :param: img_height:  The amount of pixels in height
-        :param: focal_fov: The horizontal/vertical/diagonal field of view of the lens, eg: 70h
+        :param: focal_fov: The field of view of the lens
+        :param: fov_type: Horizontal, vertical or diagonal FoV
         """
         if size == 'inch':
             sensor_size = sensor_size * 25.4
@@ -210,7 +210,7 @@ class StereoVisionCalculator(object):
         disparity_measured = disparity_real + max_disparity_error
 
         depth_measured = baseline * focal_length / disparity_measured
-        return depth_measured - depth
+        return abs(depth_measured - depth)
 
     def _callback(self):
 
@@ -226,7 +226,7 @@ class StereoVisionCalculator(object):
 
             f_mm, f_pixel = self._focalLengthCalculator(
                 sensor_size, size, img_width, img_height, focal_fov, fov_type)
-            self.l[0].set(round(f_mm, 2))
+            self.results[0].set(round(f_mm, 2))
 
             if (self.entries[4].get() and self.entries[5].get() and
                     self.entries[6].get() and self.entries[7].get()):
@@ -243,17 +243,17 @@ class StereoVisionCalculator(object):
                     max_disparity, calibration_disparity_error,
                     min_disparity)
 
-                self.l[1].set(round(baseline * 1000, 2))
-                self.l[2].set(round(min_depth * 100, 2))
+                self.results[1].set(round(baseline * 1000, 2))
+                self.results[2].set(round(min_depth * 100, 2))
 
-        if self.var[0].get():
+        if self.check[0].get():
             self.root.after(100, self._callback)
 
     def _disp(self):
-        if self.var[1].get():
-            self.entries[8]["state"] = "normal"
+        if self.check[1].get():
+            self.entries[9]["state"] = "normal"
         else:
-            self.entries[8]["state"] = "disabled"
+            self.entries[9]["state"] = "disabled"
 
     def _capture(self):
         x1 = self.root.winfo_rootx()
@@ -264,6 +264,12 @@ class StereoVisionCalculator(object):
         im = grab(bbox=(x1, y1, x2, y2))
         im.show()
 
+    def _xfrange(self, start, stop, step):
+        i = 0
+        while start + i * step < stop:
+            yield start + i * step
+            i += 1
+
     def _plot(self):
         # Parameters
         fig1, ax = plt.subplots()
@@ -271,6 +277,10 @@ class StereoVisionCalculator(object):
         plt.title("Depth Error Chart")
         plt.xlabel("Depth (m)")
         plt.ylabel("Depth error (cm)")
+        ax.set_xlim([0, 10])
+        ax.set_ylim([0, 10])
+        x1 = []
+        y1 = []
 
         # Light theme
         fig1.patch.set_facecolor('#f5f6f7')  # 212121 Dark
@@ -286,9 +296,16 @@ class StereoVisionCalculator(object):
         ax.title.set_color('#5c616c')
 
         # Plot
-        x = self.l[2].get()
-        y = self.l[3].get()
-        plt.plot(x, y)
+        # Min range to max range
+        for x in self._xfrange(1.0, 100.0, 0.1):
+            y = self._depthErrorCalculator(x, float(self.results[1].get() / 1000), float(self.results[0].get()), 0.25)
+            x1.append(x)
+            y1.append(y)
+
+        print("X: " + str(x1) + "\tY: " + str(y1))
+
+        plt.plot(x1, y1, color="b")
+        plt.legend(('Error'), loc='upper left')
 
         # Show
         plt.show()
